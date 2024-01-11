0ff9723c0fbdca0c1ac2a2351a1bd224de86368d
YuanzhiHe <heyuanzhi0728@163.com>
2024-01-11 14:02:57 +0000

    solved problems of network architecture for transfer learning

--- TL.py
+++ TL.py
@@ -14,12 +14,12 @@ env1 = Monitor(env1)
 env2 = Monitor(env2)
 
 model = TQC.load("./model/FetchReach_tqc_her2.pkl", env=env1)
-model2 = TQC.load("./model/FetchPush_tqc_her.pkl", env=env2)
+# model2 = TQC.load("./model/FetchPush_tqc_her.pkl", env=env2)
 
 
 model.policy.observation_space = env2.observation_space
-model.actor.latent_pi = model2.actor.latent_pi
-model.actor.mu = model2.actor.mu
+model.actor.latent_pi[0] = torch.nn.Linear(31, 512)
+# model.actor.mu = model2.actor.mu
 
 mean_reward, std_reward = evaluate_policy(model, env2, n_eval_episodes=10)
 
--- pushTest.py
+++ pushTest.py
@@ -1,5 +1,6 @@
 import datetime
 import gymnasium as gym
+import torch
 from sb3_contrib import TQC
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env.dummy_vec_env import DummyVecEnv
@@ -11,17 +12,11 @@ env2 = gym.make("FetchReach-v2")
 # env = DummyVecEnv([lambda: env])  # Vectorization, reducing time for future use
 env = Monitor(env)
 model = TQC.load("./model/FetchReach_tqc_her2.pkl", env=env2)
-model2 = TQC.load("./model/FetchPush_tqc_her.pkl", env=env)
 model.tensorboard_log = "./tensorboard/FetchPushTest_TQC_HER-v2/"
 model.policy.observation_space = env.observation_space
-model.actor.observation_space = env.observation_space
-model.critic.observation_space = env.observation_space
-model.policy.critic_kwargs['observation_space'] = env.observation_space
-model.critic_target.observation_space = env.observation_space
-model.env.observation_space = env.observation_space
-model.env.unwrapped.buf_obs = env.observation_space
-model.observation_space = env.observation_space
-model.learn(total_timesteps=1e6)
+model.actor.latent_pi[0] = torch.nn.Linear(31, 512)
+
+model.learn(total_timesteps=1e3)
 mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=10, render=False)
 env.close()
 print(mean_reward, std_reward)
