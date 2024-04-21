b35c3b853b1fc430819d73f4056235e4525adfaa
yuanzhihe <heyuanzhi0728@163.com>
2024-04-21 14:22:15 +0100

    final version

--- test.py
+++ test.py
@@ -4,12 +4,12 @@ from sb3_contrib import TQC
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.monitor import Monitor
 
-env_name = "FetchPickAndPlace-v2"
+env_name = "FetchSlide-v2"
 # env = gym.make(env_name, render_mode='human')
 env = gym.make(env_name)
 env = Monitor(env)
 
-model = TQC.load("./model/FetchPushToPick_tqc.pkl", env=env)
+model = TQC.load("./model/FetchPickToSlide2_tqc.pkl", env=env)
 
 mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=10)
 env.close()
